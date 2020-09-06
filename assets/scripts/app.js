d6c2dd45a7c413416d70aeda40ed6e5ccc123655
YNotPrimo <ivalinkolev0@gmail.com>
2020-09-07 02:07:34 +0300

    Connection created between the two classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,18 @@
 class Tooltip {}
 
 class ProjectItem {
-	constructor(id) {
+	constructor(id, updateProjectListsFunction) {
 		this.id = id;
+		this.updateProjectListsHandler = updateProjectListsFunction;
+
+		this.connectSwitchButton();
+		this.connectMoreInfoButton();
+	}
+	connectMoreInfoButton() {}
+	connectSwitchButton() {
+		const projectItemElement = document.getElementById(this.id);
+		const switchBtn = projectItemElement.querySelector("button:last-of-type");
+		switchBtn.addEventListener("click", this.updateProjectListsHandler);
 	}
 }
 
@@ -10,11 +20,26 @@ class ProjectList {
 	projects = [];
 
 	constructor(type) {
+		this.type = type;
+
 		const prjItems = document.querySelectorAll(`#${type}-projects li`);
 		for (const prjItem of prjItems) {
-			this.projects.push(new ProjectItem(prjItem.id));
+			this.projects.push(new ProjectItem(prjItem.id, this.moveProject.bind(this)));
 		}
 		console.log(this.projects);
+	}
+
+	setMoveHandler(moveHandlerFunction) {
+		this.moveHandler = moveHandlerFunction;
+	}
+
+	addProject() {
+		console.log(this);
+	}
+
+	moveProject(projectId) {
+		this.moveHandler(this.projects.find(p => p.id === projectId));
+		this.projects = this.projects.filter(p => p.id !== projectId);
 	}
 }
 
@@ -22,6 +47,14 @@ class App {
 	static init() {
 		const activeProjectsList = new ProjectList("active");
 		const finishedProjectsList = new ProjectList("finished");
+
+		activeProjectsList.setMoveHandler(
+			finishedProjectsList.addProject.bind(finishedProjectsList)
+		);
+
+		finishedProjectsList.setMoveHandler(
+			activeProjectsList.addProject.bind(activeProjectsList)
+		);
 	}
 }
 
