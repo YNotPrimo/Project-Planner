74b8c3c39a3c3ab59c03c95bf57dcf1dbef87460
YNotPrimo <ivalinkolev0@gmail.com>
2020-09-07 15:44:02 +0300

    Added some timeouts and intervals

--- assets/scripts/analytics.js
+++ assets/scripts/analytics.js
@@ -1,1 +1,7 @@
-console.log("asdf");
+const intervalId = setInterval(() => {
+	console.log("Sending Analytics...");
+}, 2000);
+
+document.getElementById("stop-analytics-btn").addEventListener("click", () => {
+	clearInterval(intervalId);
+});
