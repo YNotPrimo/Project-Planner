19ced768d779dc7b384055f412e2c6e1e14ff2d6
YNotPrimo <ivalinkolev0@gmail.com>
2020-09-07 14:25:18 +0300

    Showing the correct text

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -36,9 +36,10 @@ class Component {
 }
 
 class Tooltip extends Component {
-	constructor(closeNotifierFunction) {
+	constructor(closeNotifierFunction, text) {
 		super();
 		this.closeNotifier = closeNotifierFunction;
+		this.text = text;
 		this.create();
 	}
 
@@ -51,7 +52,7 @@ class Tooltip extends Component {
 		const tooltipElement = document.createElement("div");
 
 		tooltipElement.className = "card";
-		tooltipElement.textContent = "test";
+		tooltipElement.textContent = this.text;
 
 		tooltipElement.addEventListener("click", this.closeTooltip);
 
@@ -74,9 +75,13 @@ class ProjectItem {
 		if (this.hasActiveTooltip) {
 			return;
 		}
+		const projectElement = document.getElementById(this.id);
+		const tooltipText = projectElement.dataset.extraInfo;
+
 		const tooltip = new Tooltip(() => {
 			this.hasActiveTooltip = false;
-		});
+		}, tooltipText);
+
 		tooltip.attach();
 		this.hasActiveTooltip = true;
 	}
@@ -87,7 +92,7 @@ class ProjectItem {
 			"button:first-of-type"
 		);
 
-		moreInfoBtn.addEventListener("click", this.showMoreInfoHandler);
+		moreInfoBtn.addEventListener("click", this.showMoreInfoHandler.bind(this));
 	}
 
 	connectSwitchButton(type) {
